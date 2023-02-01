f4ab431536691bc06d4cefc00bb36c7a34de692f
Lokesh Tiwari <lokeshtiwari001vns@gmail.com>
2023-02-02 00:33:11 +0530

    new

--- script.js
+++ script.js
@@ -21,3 +21,54 @@ function main() {
 }
 
 img.src = "./images/fish.png"
+
+
+
+
+
+
+
+// var link = document.createElement('a')
+// var video = document.getElementById('test');
+
+
+// var videoStream = canvas.captureStream(60);
+// var mediaRecorder = new MediaRecorder(videoStream);
+
+// var chunks = []
+// mediaRecorder.ondataavailable = e => {
+//     chunks.push(e.data)
+// }
+// mediaRecorder.onstop = () => {
+//     var blob = new Blob(chunks, {'type' : 'video/mp4'});
+//     chunks = []
+//     var blobURL = URL.createObjectURL(blob);
+//     video.src = blobURL
+//     link.href = blobURL
+//     link.download = "video.mp4"
+// }
+
+
+// const save = document.getElementById('save');
+// save.addEventListener('click', () => link.click())
+
+
+// var recording = 0
+// function record(e) {
+//     if (recording) {
+//         recording = 0
+//         mediaRecorder.stop()
+        
+//         e.target.innerHTML = 'Start Recording'
+//         save.style.display = 'inline'
+//     } else {
+//         recording = 1
+//         mediaRecorder.start()
+        
+//         e.target.innerHTML = 'Stop Recording'
+//         save.style.display = 'none';
+//     }
+// }
+
+// const toggleRecord = document.getElementById('toggleRecorder')
+// toggleRecord.addEventListener('click', (e) => record(e))
