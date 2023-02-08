c7e80cd3db5490f94147e9127d0d6d4d121812eb
Lokesh Tiwari <lokeshtiwari001vns@gmail.com>
2023-02-09 04:26:57 +0530

    local debug evn initialized for this dev branch

---
+++ script.js
@@ -0,0 +1,21 @@
+import Effect from "./utils/effect.js";
+
+const canvas = document.getElementById("canvas1");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const container = document.querySelector(".controls")
+
+const effect = new Effect(canvas, container);
+
+let img = new Image();
+img.onload = main;
+function main() {
+  effect.drawImage(this);
+  effect.init();
+  effect.warp();
+  effect.initRecording(1);
+  effect.animate();
+}
+
+img.src = "./images/fish.png";
