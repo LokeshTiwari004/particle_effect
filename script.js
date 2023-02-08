2598f4e00377b6553f9b2fa73eec7a8cc03ed397
Lokesh Tiwari <lokeshtiwari001vns@gmail.com>
2023-02-08 23:34:38 +0530

    formatting code

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 import drawImage from "./utils/drawImage.js";
 import Effect from "./utils/effect.js";
 
-
-const canvas = document.getElementById('canvas1');
+const canvas = document.getElementById("canvas1");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const context = canvas.getContext("2d");
 
-const warpButton = document.getElementById('warpButton')
+const warpButton = document.getElementById("warpButton");
 
 let img = new Image();
 img.onload = main;
 function main() {
-    const imgInfo = drawImage(this, canvas, context);
-    const effect = new Effect(context, canvas, imgInfo);
-    effect.init();
-    effect.warp(warpButton);
-    effect.initRecording(document.querySelector('.controls'), 1)
-    effect.animate();
+  const imgInfo = drawImage(this, canvas, context);
+  const effect = new Effect(context, canvas, imgInfo);
+  effect.init();
+  effect.warp(warpButton);
+  effect.initRecording(document.querySelector(".controls"), 1);
+  effect.animate();
 }
 
-img.src = "./images/fish.png"
+img.src = "./images/fish.png";
